75745d2e4ffd80ef75ef643834bcb496eb2c5e73
marko-zc <marko.zarkovic.cerneli@gmail.com>
2021-03-07 21:18:22 +0100

    Updates

---
+++ precache-manifest.5f4df08de3405bcc44645a9db1307736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85b6770cb2278308694fa34a3ab77914",
+    "url": "/Simple-Calculator/index.html"
+  },
+  {
+    "revision": "f4f2939fd18fb1f40ccb",
+    "url": "/Simple-Calculator/static/css/main.c4bfa2a4.chunk.css"
+  },
+  {
+    "revision": "9c5fffedc6fc2c3a1c93",
+    "url": "/Simple-Calculator/static/js/2.26ae4dbd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Simple-Calculator/static/js/2.26ae4dbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4f2939fd18fb1f40ccb",
+    "url": "/Simple-Calculator/static/js/main.10e5d29b.chunk.js"
+  },
+  {
+    "revision": "74068f36181c1151e082",
+    "url": "/Simple-Calculator/static/js/runtime-main.6a1cde54.js"
+  }
+]);
